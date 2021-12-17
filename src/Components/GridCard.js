94301fcbc601a2f8551453085db66c4c4b5af254
Pranay Lad <pranay@pakkaprofile.in>
2021-12-17 09:13:54 +0530

    Created the layout for the cards

--- src/Components/GridCard.js
+++ src/Components/GridCard.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-function GridCard() {
+function GridCard(props) {
     return (
-        <div>
+        <div style={{
+            ...styles.card,
+            ...styles[props.size]
+            }}>
             
         </div>
     )
 }
 
-export default GridCard
+const styles = {
+    card: {
+    margin: '15px',
+    padding: 0,
+    borderRadius: '16px',
+    backgroundColor: 'lightPink'
+    },
+    small: {
+    gridRowEnd: 'span 16'
+    },
+    medium: {
+    gridRowEnd: 'span 25'
+    },
+    large: {
+    gridRowEnd: 'span 30'
+    }
+   }
+export default GridCard;
--- src/Screens/PinterestGridScreen.js
+++ src/Screens/PinterestGridScreen.js
@@ -1,11 +1,41 @@
 import React from 'react'
-
+import GridCard from '../Components/GridCard'
 function PinterestGridScreen() {
+    
     return (
-        <div>
-            
+        <div style={styles.mainContainer}>
+            <GridCard size="small"/>
+            <GridCard size="medium"/>
+            <GridCard size="large"/>
+            <GridCard size="small"/>
+            <GridCard size="medium"/>
+            <GridCard size="large"/>
+            <GridCard size="small"/>
+            <GridCard size="medium"/>
+            <GridCard size="large"/>
+            <GridCard size="large"/>
+            <GridCard size="small"/>
+            <GridCard size="medium"/>
+            <GridCard size="large"/>
+            <GridCard size="small"/>
+            <GridCard size="medium"/>
+            <GridCard size="large"/>
         </div>
     )
 }
 
-export default PinterestGridScreen
+const styles={
+    mainContainer:{
+        margin:0,
+        padding:40,
+        width:'80vw',
+        position:'absolute',
+        left:'50%',
+        transform:'translateX(-46%)',
+        display:'grid',
+        gridTemplateColumns:'repeat(auto-fill,260px)',
+        gridAutoRows:'16px'
+    }
+}
+
+export default PinterestGridScreen;
